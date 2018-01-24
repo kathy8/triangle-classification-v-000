5336ef1f72310ee52c51491e1f905a12916ea9e4
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2018-01-24 19:28:08 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,7 +1,7 @@
 require 'pry'
 class Triangle
   attr_accessor :side_1, :side_2, :side_3
-end 		
+
 def initialize(side_1, side_2, side_3)
 @side_1 = side_1
 @side_2 = side_2
@@ -30,5 +30,5 @@ class TriangleError < StandardError
 
 end
 
-  
+
 end
