f4ba5a9e9d377854f8e658772d8ddc980224e7a1
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2018-01-24 19:32:36 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,33 +1,30 @@
-require 'pry'
+require "pry"
 class Triangle
-  attr_accessor :side_1, :side_2, :side_3
 
 def initialize(side_1, side_2, side_3)
-@side_1 = side_1
-@side_2 = side_2
-@side_3 = side_3
+@sides = []
+@side_1 << side_1
+@side_2 << side_2
+@side_3 << side_3
 end
 
 def kind
-if @side_1 <= 0 || @side_2 <= 0 || @side_3 <= 0
-raise TriangleError
-elsif @side_1 + @side_2 < @side_3 || @side_1 + @side_3 == @side_2 || @side_2 + @side_3 < @side_1
-raise TriangleError
-end
-if @side_1 == @side_2 && @side_1 == @side_3
-:equilateral
- end
+  if @sides.include?(0) || @sides[0] + @sides[1] <= @sides[2] || @sides[0] + @sides[2] <= @sides[1] || @sides[2] + @sides[1] <= @sides[0]
+   raise TriangleError
+   elsif @sides[0] == @sides[1] && @sides[0] == @sides[2]
+   :equilateral
+   elsif @sides[0] == @sides[1] || @sides[0] == @sides[2] || @sides[1] == @sides[2]
+   :isosceles
+   else :scalene
+   end
+    end
 
-elsif @side_2 == @side_3 || @side_1 == @side_3 || @side_1 == @side_2
-:isosceles
-elsif @side_1 != @side_2 && @side_1 != @side_3
-:scalene
-end
-end
-end
+  end
 
 class TriangleError < StandardError
-
+def message
+  "There is an eror with the sides of your triangle!"
+end
 end
 
 
